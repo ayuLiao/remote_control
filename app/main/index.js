3a7ba7daa72ec1539fbfa3d205c385ed0c0ba3dc
liaomaowen <liaomw@huitouche.com>
2020-12-10 10:40:50 +0800

    使用concurrently和wait-on，简化启动流程

--- app/main/index.js
+++ app/main/index.js
@@ -1,4 +1,6 @@
 const {app, BrowserWindow} = require('electron')
+const isDev = require('electron-is-dev')
+const path = require('path')
 
 let win
 
@@ -10,5 +12,13 @@ app.on('ready', () => {
             nodeIntegration: true
         }
     })
-    win.loadURL('http://localhost:3000')
+    if (isDev) {
+        // 开发环境
+        win.loadURL('http://localhost:3000')
+    } else {
+        // path.resolve()会把一个路径或路径片段的序列解析为一个绝对路径
+        // __dirname 当前文件的目录名
+        // 我们的项目，会将最终的html页面打包生成在pages/main下
+        win.loadFile(path.resolve(__dirname, '../renderer/pages/main/index.html'))
+    }
 })
